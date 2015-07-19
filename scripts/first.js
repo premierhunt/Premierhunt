12331677204e1093d82215cd943777aae563703c
tusharkakkar90 <tusharkakkar90@gmail.com>
2015-07-19 15:21:58 +0530

    Change the color and add icons

---
+++ scripts/first.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("a.mobile").click(function()
+	{
+		$(".sidebar").slideToggle('fast');
+	});
+	window.onresize = function(events){
+		if($(window).width() > 320){
+			$(".sidebar").show();
+		}
+		else {
+			$(".sidebar").hide();
+		}
+	}
+});
